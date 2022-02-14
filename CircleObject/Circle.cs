b486f63542bd9432354ef23e7098de4215a9da6b
lbandy618 <97638884+lbandy618@users.noreply.github.com>
2022-02-14 10:03:40 -0500

    initial commit

---
+++ CircleObject/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObject
+{
+    internal class Circle
+    {
+        public double Radius { get; set; }
+        
+        //constructor
+        public Circle(double _Radius)
+        {
+            Radius = _Radius;
+        }
+        //methods
+        public double CalculateCircumfrence()
+        {
+            return (2 * Math.PI) * Radius;
+        }
+        public double CalculateArea()
+        {
+            return (Math.PI * Math.Pow(Radius, 2));
+        }
+        private string FormatNumber(double x)
+        {
+            double rounded = Math.Round(x, 2);
+            return rounded.ToString();             
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+        public string CalculateFormattedCircumfrence()
+        {
+            return FormatNumber(CalculateCircumfrence());
+        }
+    }
+}
---
+++ CircleObject/Program.cs
@@ -0,0 +1,60 @@
+﻿using CircleObject;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runProgram = true;
+            List<Circle> myCircles = new List<Circle>();
+
+            while (runProgram)
+            {
+                Console.Write("Please enter a radius: ");
+
+                double radius;
+
+
+                while (!double.TryParse(Console.ReadLine(), out radius))
+                {
+                    Console.Write("Not a valid side length, please try again: ");
+                }
+
+                Circle circle = new Circle(radius);
+
+                Console.WriteLine("Circumfrence: " + circle.CalculateFormattedCircumfrence());
+                Console.WriteLine("Area: " + circle.CalculateFormattedArea());
+                myCircles.Add(circle);
+
+                runProgram = GetContinue();
+            }
+            Console.WriteLine($"Goodbye. You created {myCircles.Count} circles.");
+        }
+        static bool GetContinue()
+        {
+            bool result = true;
+            while (true)
+            {
+                Console.Write("Do you want to continue? y/n: ");
+                string choice = Console.ReadLine().Trim().ToLower();
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == "n")
+                {
+                    result = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not valid.");
+                }
+            }
+            return result;
+        }
+    }
+}
